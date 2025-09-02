8ff4039c502231f388a70f14f9e640c4803d198b
bhargaviamidhepuram <162031508+bhargaviamidhepuram@users.noreply.github.com>
2025-09-02 22:14:53 +0530

    Time: 0 ms (100%), Space: 44.1 MB (97.4%) - LeetHub

---
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int i = 1; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(j - 1 < 0){
+                    continue;
+                }
+                else if(matrix[i][j] != matrix[i - 1][j - 1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
